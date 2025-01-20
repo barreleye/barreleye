cda69141f4a6dbb8daa1b983f5d5d01f2c7e806a
Ivan Akimov <ivan@barreleye.com>
2025-01-19 19:06:45 -0600

    wip

--- common/src/db/migrations/m20240101_000004_create_api_keys.rs
+++ common/src/db/migrations/m20240101_000004_create_api_keys.rs
@@ -1,8 +1,6 @@
 use async_trait::async_trait;
 use sea_orm_migration::prelude::*;
 
-use crate::{models::ApiKey, utils, IdPrefix};
-
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
@@ -32,28 +30,6 @@ impl MigrationTrait for Migration {
 							.not_null()
 							.extra("DEFAULT CURRENT_TIMESTAMP".to_owned()),
 					)
-					.to_owned(),
-			)
-			.await?;
-
-		let (secret_key, secret_key_hash) = ApiKey::generate_key();
-		manager
-			.exec_stmt(
-				Query::insert()
-					.into_table(ApiKeys::Table)
-					.columns([
-						ApiKeys::Id,
-						ApiKeys::SecretKey,
-						ApiKeys::SecretKeyHash,
-						ApiKeys::IsActive,
-					])
-					.values_panic([
-						utils::unique_id(IdPrefix::ApiKey, "default").into(),
-						secret_key.into(),
-						secret_key_hash.into(),
-						true.into(),
-					])
-					.on_conflict(OnConflict::columns([ApiKeys::Id]).do_nothing().to_owned())
 					.to_owned(),
 			)
 			.await
